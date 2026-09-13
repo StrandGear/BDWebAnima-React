c1f1f8475c76a468c6c58ffcf6507fa86c9f5871
StrandGear <strandgear@mail.ru>
2026-09-13 21:36:25 +0200

    new unity build files

--- src/screens/Mobile_Bd/Bd.jsx
+++ src/screens/Mobile_Bd/Bd.jsx
@@ -14,9 +14,9 @@ export const Bd = () => {
 
   const { unityProvider } = useUnityContext({
     loaderUrl: "/BDWebAnima-React/unity/Build/9edd899bc6b6e0bbc4f46ff33ca0bba6.loader.js",
-    dataUrl: "/BDWebAnima-React/unity/Build/9a6aeb4cf4dcafeee9a1d054dc7408fa.data",
-    frameworkUrl: "/BDWebAnima-React/unity/Build/a29c1b8001122d8afb2a4eebb2644813.framework.js",
-    codeUrl: "/BDWebAnima-React/unity/Build/4c54c85938a146a1353ebe1dcb687fba.wasm",
+    dataUrl: "/BDWebAnima-React/unity/Build/c52841c9150b40a1a91d58d841fd5675.data",
+    frameworkUrl: "/BDWebAnima-React/unity/Build/005d4f487ee79e378e58dbac699c4593.framework.js",
+    codeUrl: "/BDWebAnima-React/unity/Build/5f016967b961d2540b171381efa4120a.wasm",
     streamingAssetsUrl: "/BDWebAnima-React/unity/StreamingAssets"
   });
 
--- src/screens/UnityBuildingdemocracy/UnityBuildingdemocracy.jsx
+++ src/screens/UnityBuildingdemocracy/UnityBuildingdemocracy.jsx
@@ -17,9 +17,9 @@ export const UnityBuildingdemocracy = () => {
 
   const { unityProvider } = useUnityContext({
     loaderUrl: "../../../public/unity/Build/9edd899bc6b6e0bbc4f46ff33ca0bba6.loader.js",
-    dataUrl: "../../../public/unity/Build/9a6aeb4cf4dcafeee9a1d054dc7408fa.data",
-    frameworkUrl: "../../../public/unity/Build/a29c1b8001122d8afb2a4eebb2644813.framework.js",
-    codeUrl: "../../../public/unity/Build/4c54c85938a146a1353ebe1dcb687fba.wasm",
+    dataUrl: "../../../public/unity/Build/c52841c9150b40a1a91d58d841fd5675.data",
+    frameworkUrl: "../../../public/unity/Build/005d4f487ee79e378e58dbac699c4593.framework.js",
+    codeUrl: "../../../public/unity/Build/5f016967b961d2540b171381efa4120a.wasm",
     streamingAssetsUrl: "../../../public/unity/StreamingAssets"
   });
 

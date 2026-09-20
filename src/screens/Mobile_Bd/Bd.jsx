c60b3c3f31f919a94d77e7b5f56718a663b20c22
StrandGear <strandgear@mail.ru>
2026-09-20 09:35:26 +0200

    bd resolution

--- src/screens/Mobile_Bd/Bd.jsx
+++ src/screens/Mobile_Bd/Bd.jsx
@@ -30,25 +30,8 @@ export const Bd = () => {
         </div>
       </div>
 
-      {/* --- STRICTLY POSITIONED UNITY CONTAINER --- */}
-      <div 
-        className="unity-mobile-container" 
-        style={{
-          position: "absolute",
-          top: "15%", // Pushes it down below your top toggle bar
-          left: "50%",
-          transform: "translateX(-50%)", // Centers it perfectly
-          width: "340px", // Set this to the exact width you want for the game
-          height: "730px", // Set this to the exact height you want
-          zIndex: 5, // Keeps it as a background layer
-          borderRadius: "30px", // Gives it rounded "phone" corners
-          overflow: "hidden", // Ensures the game doesn't bleed out of the rounded corners
-          backgroundColor: "#1C1C1C", // A dark placeholder background while loading
-          display: "flex", 
-          justifyContent: "center", 
-          alignItems: "center"
-        }}
-      >
+      {/* Responsive Unity WebGL Container */}
+      <div className="unity-mobile-container">
         {isFirebaseReady ? (
           <Unity 
             unityProvider={unityProvider} 
@@ -63,26 +46,26 @@ export const Bd = () => {
       <div className="rechteck-2" />
       
       {/* Top Toggle */}
-      <Link className="gruppe-2" to="/ri" style={{ zIndex: 20 }}>
+      <Link className="gruppe-2" to="/ri">
         <div className="text-wrapper">BUILDING DEMOCRACY</div>
-        <div className="text-wrapper-2">REMOTE ISLAND</div>
         <div className="schalter">
           <div className="uncheck" />
         </div>
+        <div className="text-wrapper-2">REMOTE ISLAND</div>
       </Link>
       
       <div className="NS-dok-logo" />
       
-      <Link className="text-wrapper-3" to="/mitwirkende" style={{ zIndex: 20 }}>
+      <Link className="text-wrapper-3" to="/mitwirkende">
         Impressum
       </Link>
-      <Link className="text-wrapper-4" to="/impressum" style={{ zIndex: 20 }}>
+      <Link className="text-wrapper-4" to="/impressum">
         Mitwirkende
       </Link>
       
-      {/* --- ARROWS AND TEXT GUARANTEED TO BE ON TOP --- */}
-      <Link className="gruppe-3" to="/bd-1" style={{ zIndex: 20, position: "absolute" }}>
-        <img className="polygon" alt="Polygon" src="/img/polygon-1-4.png" />
+      {/* Side Control Links */}
+      <Link className="gruppe-3" to="/bd-1">
+        <img className="polygon" alt="Previous" src="/img/polygon-1-4.png" />
         <div className="ergebnis-beitr-ge">
           Ergebnis-
           <br />
@@ -90,13 +73,13 @@ export const Bd = () => {
         </div>
       </Link>
       
-      <Link className="gruppe-4" to="/bd-2" style={{ zIndex: 20, position: "absolute" }}>
+      <Link className="gruppe-4" to="/bd-2">
         <div className="spielaleitung">
           Spielaleitung
           <br />
           Download
         </div>
-        <img className="polygon" alt="Polygon" src="/img/polygon-3-3.png" />
+        <img className="polygon" alt="Next" src="/img/polygon-3-3.png" />
       </Link>
     </div>
   );

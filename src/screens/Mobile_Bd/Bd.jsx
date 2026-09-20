98c7f4701cc5a97e29fbf0cdb18bda446907ee87
StrandGear <strandgear@mail.ru>
2026-09-20 12:40:08 +0200

    img paths

--- src/screens/Mobile_Bd/Bd.jsx
+++ src/screens/Mobile_Bd/Bd.jsx
@@ -75,8 +75,8 @@ export const Bd = () => {
   }, []);
 
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/BDWebAnima-React/unity/Build/9edd899bc6b6e0bbc4f46ff33ca0bba6.loader.js",
-    dataUrl: "/BDWebAnima-React/unity/Build/c52841c9150b40a1a91d58d841fd5675.data",
+     loaderUrl: "/BDWebAnima-React/unity/Build/9edd899bc6b6e0bbc4f46ff33ca0bba6.loader.js",
+        dataUrl: "/BDWebAnima-React/unity/Build/c52841c9150b40a1a91d58d841fd5675.data",
     frameworkUrl: "/BDWebAnima-React/unity/Build/005d4f487ee79e378e58dbac699c4593.framework.js",
     codeUrl: "/BDWebAnima-React/unity/Build/5f016967b961d2540b171381efa4120a.wasm",
     streamingAssetsUrl: "/BDWebAnima-React/unity/StreamingAssets"
